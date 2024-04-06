433521565ef77234497a95027a96325518923375
sudarsana-reddy <sudarsana.ala@gmail.com>
2024-04-06 10:49:56 +0530

    update to 1.0.1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const calculator = require('@sud/npm-proj');
 
-console.log(calculator.add(5,-4));
+console.log( `Addition of 5 and -4 : ${calculator.add(5,-4)}`);
+console.log( `Subtract of 5 and -4 : ${calculator.add(5,-4)}`);
