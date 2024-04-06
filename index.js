57aaf9a40e810578af8a3858511e8acaf73c64df
sudarsana-reddy <sudarsana.ala@gmail.com>
2024-04-06 10:53:18 +0530

    added subtract

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const calculator = require('@sud/npm-proj');
 
 console.log( `Addition of 5 and -4 : ${calculator.add(5,-4)}`);
-console.log( `Subtract of 5 and -4 : ${calculator.add(5,-4)}`);
+console.log( `Subtract of 5 and -4 : ${calculator.sub(5,-4)}`);
